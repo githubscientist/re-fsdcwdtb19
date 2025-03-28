e9d49eeacf87fe2692306bf60dba133887d511f1
githubscientist <sathish.datascientist@gmail.com>
2025-03-28 20:18:05 +0530

    creating our first component in React --after a basic cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <p>Hello World, Thanks for visiting my Webpage!</p>
+    </>
+  )
+}
+
+createRoot(document.getElementById('root'))
+  .render(<App />);
