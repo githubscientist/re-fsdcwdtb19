2cf110544d9a06b5bd7a014004d6feb1ff2dd695
githubscientist <sathish.datascientist@gmail.com>
2025-04-01 18:52:16 +0530

    running js statements in a component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import Header from "./components/Header";
 
 // Base Component
 function App() {
+
+    let birthdate = new Date("1990-01-04");
+    let today = new Date();
+
+    let age = today.getFullYear() - birthdate.getFullYear();
+
+    console.log(age);
+
+
     return (
         <>
             <Header />
