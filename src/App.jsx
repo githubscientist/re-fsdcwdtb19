a3b84386dc6f0bd5152d69adf9514dbeba6a0ce8
githubscientist <sathish.datascientist@gmail.com>
2025-04-01 21:07:35 +0530

    useState hook --simple counter example

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
+
 const App = () => {
 
-    let count = 0;
+    let [count, setCount] = useState(0);
 
+    // state change causes re-render
     function handleClick() {
-        count++; // count = count + 1
-        console.log(count);
+        setCount(count + 1);
     }
 
     return (
