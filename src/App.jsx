48f02048b407d58dbbf1b5baa2d67d67cf82754c
githubscientist <sathish.datascientist@gmail.com>
2025-04-02 19:25:17 +0530

    class components --an overview

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,69 @@
-import { useState } from "react";
+// Functional Component: Stateless Component
+// const App = () => {
+//     return (
+//         <div>App</div>
+//     )
+// }
 
-const App = () => {
+// Class Component: Stateful Component
+import React from "react";
 
-    const [reactions, setReactions] = useState({
-        likes: 0,
-        dislikes: 0
-    });
+class App extends React.Component {
+    // constrctor: a special method that is called when an instance or object of the class is created
+    constructor(props) {
+        super(props); // calls the constructor of the parent class React
 
-    function handleLike() {
-        setReactions({
-            ...reactions,
-            likes: reactions.likes + 1,
+        this.state = {
+            count: 0,
+            isVisible: true
+        }
+    }
+
+    handleIncrement = () => {
+        // this.setState((prevState) => ({
+        //     count: prevState.count + 1
+        // }));
+
+        this.setState({
+            count: this.state.count + 1
         })
     }
 
-    function handleDislike() {
-        setReactions({
-            ...reactions,
-            dislikes: reactions.dislikes + 1
-        })
+    // lifecycle methods
+    componentDidMount() {
+        // this is called when the component is mounted
+        console.log("Component mounted");
     }
 
-    return (
-        <>
-            <button onClick={handleLike}>Like</button> {reactions.likes} likes
-            <br /><br />
-            <button onClick={handleDislike}>Dislike</button> {reactions.dislikes} dislikes
-        </>
-    )
+    componentDidUpdate() {
+        // this is called when the component is updated
+        console.log("Component updated");
+    }
+
+    componentWillUnmount() {
+        // this is called when the component is unmounted
+        console.log("Component unmounted");
+    }
+
+    render() {
+        return (
+            <>
+                <button
+                    onClick={() => this.setState({ ...this.state, isVisible: !this.state.isVisible })}
+                >Hide</button>
+                {
+                    this.state.isVisible && (
+                        <div>
+                            <h1>Count: {this.state.count}</h1>
+                            <button
+                                onClick={this.handleIncrement}
+                            >Increment</button>
+                        </div>
+                    )
+                }
+            </>
+        )
+    }
 }
 
 export default App;
