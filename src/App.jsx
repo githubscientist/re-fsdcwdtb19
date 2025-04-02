baffffda477f3f9deed227b6875792aa7261324d
githubscientist <sathish.datascientist@gmail.com>
2025-04-02 18:21:51 +0530

    using multiple states

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,22 @@ import { useState } from "react";
 
 const App = () => {
 
-    let [count, setCount] = useState(0);
+    const [likes, setLikes] = useState(0);
+    const [dislikes, setDislikes] = useState(0);
 
-    // state change causes re-render
-    function handleClick() {
-        setCount(count + 1);
+    function handleLike() {
+        setLikes(likes + 1);
+    }
+
+    function handleDislike() {
+        setDislikes(dislikes + 1);
     }
 
     return (
         <>
-            <h1>Count: {count}</h1>
-            <button
-                onClick={handleClick}
-            >Increase</button>
+            <button onClick={handleLike}>Like</button> {likes} likes
+            <br /><br />
+            <button onClick={handleDislike}>Dislike</button> {dislikes} dislikes
         </>
     )
 }
