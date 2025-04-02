d4f0a07bbe5a675e16b3d402d18116f873c27f5e
githubscientist <sathish.datascientist@gmail.com>
2025-04-02 19:39:06 +0530

    useEffect --with empty deps list

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,31 @@ import { useEffect, useState } from "react";
 
 const App = () => {
 
-    const [count, setCount] = useState(0);
+    const [positives, setPositives] = useState(0);
+    const [negatives, setNegatives] = useState(0);
 
     // this runs when the component mounts
     // it will also run when the component updates
+    // useEffect(() => {
+    //     console.log('App mounted');
+    // });
+
+    // this runs when the component mounts and only once
     useEffect(() => {
         console.log('App mounted');
-    });
+    }, []);
 
     return (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-        </div>
+        <>
+            <div>
+                <h1>Positives: {positives}</h1>
+                <button onClick={() => setPositives(positives + 1)}>Positive Inc</button>
+            </div>
+            <div>
+                <h1>Negatives: {negatives}</h1>
+                <button onClick={() => setNegatives(negatives + 1)}>Negative Inc</button>
+            </div>
+        </>
     )
 }
 
