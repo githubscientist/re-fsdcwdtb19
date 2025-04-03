c701c1618890c385a569968a6aa09dd233537920
githubscientist <sathish.datascientist@gmail.com>
2025-04-03 18:25:12 +0530

    keys in arrays

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 // Promise Chaining: Promise chaining is a technique used to handle asynchronous operations in JavaScript. It allows you to execute a series of asynchronous tasks in a sequence, where each task depends on the result of the previous one.
 // It is achieved by returning a new promise from the `then` method of the previous promise.
 
+// Keys in Array: In React, when rendering a list of elements, each element should have a unique key prop. This helps React identify which items have changed, are added, or are removed. Keys should be stable, predictable, and unique to the list.
+
 const App = () => {
 
     const [posts, setPosts] = useState([]);
@@ -38,8 +40,8 @@ const App = () => {
         <>
             <h1>Posts</h1>
             {
-                posts.map(post => {
-                    return <div>
+                posts.map((post, index) => {
+                    return <div key={index}>
                         <h3>{post.id}: {post.title}</h3>
                         <p><em>{post.body}</em></p>
                         <hr />
