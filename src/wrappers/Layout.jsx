ab2aec2c4a1d222c39f60ab5ce17ab778b4823ca
githubscientist <sathish.datascientist@gmail.com>
2025-04-04 19:41:19 +0530

    Navbar updated

--- src/wrappers/Layout.jsx
+++ src/wrappers/Layout.jsx
@@ -1,13 +1,10 @@
 import { Outlet } from "react-router";
+import NavBar from "../components/NavBar";
 
 const Layout = () => {
     return (
         <div>
-            <nav>
-                <li><a href="/">Home</a></li>
-                <li><a href="/register">Register</a></li>
-                <li><a href="/login">Login</a></li>
-            </nav>
+            <NavBar />
             <div>
                 <Outlet />
             </div>
