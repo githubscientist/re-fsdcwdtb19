c0b4031d1dd902bd95c12fb0aa0d71c05d3d71f5
githubscientist <sathish.datascientist@gmail.com>
2025-04-04 20:58:58 +0530

    decrement button added

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,31 @@ import { useReducer } from "react";
 function reducer(state, action) {
     if (action.type === 'INCR') {
         return state + 1;
+    } else if (action.type === 'DECR') {
+        return state - 1;
     }
 
     return state;
 }
 
-
 const App = () => {
 
     const [state, dispatch] = useReducer(reducer, 0);
 
-    function handleClick() {
+    function handleIncrement() {
         dispatch({ type: 'INCR' });
+    }
+
+    function handleDecrement() {
+        dispatch({ type: 'DECR' });
     }
 
     return (
         <div>
             <h1>Count: {state}</h1>
-            <button onClick={handleClick}>Increment</button>
+            <button onClick={handleIncrement}>Increment</button>
+            <br /><br />
+            <button onClick={handleDecrement}>Decrement</button>
         </div>
     )
 }
