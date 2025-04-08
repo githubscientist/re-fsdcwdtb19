36172fac67c83fad60200d425d9b2a556f8a520c
githubscientist <sathish.datascientist@gmail.com>
2025-04-08 19:17:09 +0530

    URL Params: linking a component for the route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Notes from "./pages/Notes";
 import notesLoader from "./loaders/unit/notesLoader";
+import Note from "./pages/Note";
 
-const router = createBrowserRouter([
+const routes = [
     {
         path: "/",
         element: <Notes />,
         loader: notesLoader,
         hydrateFallbackElement: <div>Loading...</div>,
+    },
+    {
+        path: "/note/:id",
+        element: <Note />
     }
-], {
+];
+
+const router = createBrowserRouter(routes, {
     future: {
         v7_relativeSplatPath: true,
         v7_fetcherPersist: true,
