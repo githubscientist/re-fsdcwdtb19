260f3e6dca2c045274b612863f841ef78ba4a711
githubscientist <sathish.datascientist@gmail.com>
2025-04-08 19:31:36 +0530

    URL Params: getting params in the component and rendering the data --useEffect API

--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
 const Note = () => {
+
+    const [note, setNote] = useState({});
+
+    // read the params id from the URL
+    const { id } = useParams();
+
+    useEffect(() => {
+        fetch(`https://67f3ed22cbef97f40d2cb280.mockapi.io/notes/${id}`)
+            .then(response => response.json())
+            .then(data => setNote(data));
+    }, []);
+
+    console.log(note);
+
     return (
-        <div>Note</div>
+        <div>
+            <h2>{note.title}</h2>
+            <p>{note.description}</p>
+        </div>
     )
 }
 
