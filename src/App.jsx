ffa02d85d9bb1957b8c26bd5b4380577da1ef1ba
githubscientist <sathish.datascientist@gmail.com>
2025-04-09 20:34:56 +0530

    basic project cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,7 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
-import Notes from "./pages/Notes";
-import notesLoader from "./loaders/unit/notesLoader";
-import Note from "./pages/Note";
-import noteLoader from "./loaders/unit/noteLoader";
-import New from "./pages/New";
-import Edit from "./pages/Edit";
-
-const routes = [
-    {
-        path: "/",
-        element: <Notes />,
-        loader: notesLoader,
-        hydrateFallbackElement: <div>Loading...</div>,
-    },
-    {
-        path: "/note",
-        element: <Note />,
-        loader: noteLoader,
-        hydrateFallbackElement: <div>Loading...</div>,
-    },
-    {
-        path: "/new",
-        element: <New />
-    },
-    {
-        path: "/edit",
-        element: <Edit />,
-        loader: noteLoader,
-        hydrateFallbackElement: <div>Loading...</div>,
-    }
-];
-
-const router = createBrowserRouter(routes, {
-    future: {
-        v7_relativeSplatPath: true,
-        v7_fetcherPersist: true,
-        v7_normalizeFormMethod: true,
-        v7_partialHydration: true,
-        v7_skipActionErrorRevalidation: true,
-    }
-});
-
 const App = () => {
-    return <RouterProvider
-        router={router}
-        future={{
-            v7_startTransition: true,
-        }}
-    />;
+    return (
+        <div>App</div>
+    )
 }
 
 export default App;
