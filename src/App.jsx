175d511d406c4dc7435f4beb042736ea9a41836d
githubscientist <sathish.datascientist@gmail.com>
2025-04-10 18:36:53 +0530

    counter --useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,19 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
-import Notes from "./pages/Notes";
-import notesLoader from "./loaders/unit/notesLoader";
-import Note from "./pages/Note";
-import noteLoader from "./loaders/unit/noteLoader";
-import New from "./pages/New";
-import Edit from "./pages/Edit";
-
-const routes = [
-    {
-        path: "/",
-        element: <Notes />,
-        loader: notesLoader,
-        hydrateFallbackElement: <div>Loading...</div>,
-    },
-    {
-        path: "/note",
-        element: <Note />,
-        loader: noteLoader,
-        hydrateFallbackElement: <div>Loading...</div>,
-    },
-    {
-        path: "/new",
-        element: <New />
-    },
-    {
-        path: "/edit",
-        element: <Edit />,
-        loader: noteLoader,
-        hydrateFallbackElement: <div>Loading...</div>,
-    }
-];
-
-const router = createBrowserRouter(routes, {
-    future: {
-        v7_relativeSplatPath: true,
-        v7_fetcherPersist: true,
-        v7_normalizeFormMethod: true,
-        v7_partialHydration: true,
-        v7_skipActionErrorRevalidation: true,
-    }
-});
+import { useState } from "react";
 
 const App = () => {
-    return <RouterProvider
-        router={router}
-        future={{
-            v7_startTransition: true,
-        }}
-    />;
+
+    const [count, setCount] = useState(0);
+
+    const handleIncrement = () => {
+        setCount(count + 1);
+    }
+
+    return (
+        <div>
+            <h1>Counter: {count}</h1>
+            <button onClick={handleIncrement}>Increment</button>
+        </div>
+    )
 }
 
 export default App;
--- src/loaders/unit/notesLoader.js
+++
@@ -1,8 +0,0 @@
-import noteServices from "../../services/noteServices";
-
-const notesLoader = async () => {
-    const response = await noteServices.getAllNotes();
-    return response.data;
-}
-
-export default notesLoader;
